ab59f1daffc93e8061196577254aca7d1be532a1
malamanson <malamanson@hotmail.com>
2020-06-17 20:05:09 -0400

    Correcao gerar CPF e CNPJ invalido

--- SharedObjects/GeradorCNPJCPF.cs
+++ SharedObjects/GeradorCNPJCPF.cs
@@ -10,73 +10,6 @@ namespace Lampp.CAPDA.Teste.Automatizado.SharedObjects
         Int64[] numero = new Int64[13];
         Random rand = new Random();
 
-        public String CpfComMascara(Int64 quant)
-        {
-            String result = "";
-            for (cpf = 1; cpf <= quant; cpf++)
-            {
-                for (i = 1; i <= 9; i++)
-                {
-                    erro = 1;
-                    do
-                    {
-                        if (erro > 1)
-                        {
-                            erro = 1;
-                        }
-                        numero[i] = (rand.Next()) % 9;
-                        erro++;
-                    } while (numero[i] > 9 || numero[i] < 0);
-                }
-                //*==========================================*
-                //|       Primeiro digito verificador        |
-                //*==========================================*
-                soma1 = ((numero[1] * 10) +
-                      (numero[2] * 9) +
-                      (numero[3] * 8) +
-                      (numero[4] * 7) +
-                      (numero[5] * 6) +
-                      (numero[6] * 5) +
-                      (numero[7] * 4) +
-                      (numero[8] * 3) +
-                      (numero[9] * 2));
-                parte1 = (soma1 / 11);
-                parte2 = (parte1 * 11);
-                parte3 = (soma1 - parte2);
-                dig1 = (11 - parte3);
-                if (dig1 > 9) dig1 = 0;
-                //*==========================================*
-                //|        Segundo digito verificador        |
-                //*==========================================*
-                soma2 = ((numero[1] * 11) +
-                      (numero[2] * 10) +
-                      (numero[3] * 9) +
-                      (numero[4] * 8) +
-                      (numero[5] * 7) +
-                      (numero[6] * 6) +
-                      (numero[7] * 5) +
-                      (numero[8] * 4) +
-                      (numero[9] * 3) +
-                      (dig1 * 2));
-                parte5 = (soma2 / 11);
-                parte6 = (parte5 * 11);
-                parte7 = (soma2 - parte6);
-                dig2 = (11 - parte7);
-                if (dig2 > 9) dig2 = 0;
-                //*==========================================*
-                //|       Impressao do numero completo       | 
-                //*==========================================*
-                for (i = 1; i <= 9; i++)
-                {
-                    result += Convert.ToString(numero[i]);
-                    if (i - 1 == 2) result += ".";
-                    if (i - 1 == 5) result += ".";
-                }
-                result += "-" + dig1 + dig2; // dois últimos digitos
-            }
-            return result;
-        }
-
         public String CpfSemMascara(Int64 quant)
         {
             String result = "";
@@ -142,6 +75,12 @@ namespace Lampp.CAPDA.Teste.Automatizado.SharedObjects
                 // dois últimos digitos
                 result += dig1 + "" + dig2;
             }
+
+            //Äs vezes gera um CPF inválido, não sei o motivo. Se for inválido, faz de novo
+            if (!ValidarCPF(result))
+            {
+                result = CpfSemMascara(1);
+            }
             return result;
         }
 
@@ -176,10 +115,88 @@ namespace Lampp.CAPDA.Teste.Automatizado.SharedObjects
 
 
             string cnpj = string.Concat(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, d1, d2);
+
+            //Äs vezes gera um CNPJ inválido, não sei o motivo. Se for inválido, faz de novo
+            if (!ValidaCNPJ(cnpj))
+            {
+               cnpj = GerarCNPJ();
+            }
             return cnpj;
+
         }
-            
+
+        public static bool ValidaCNPJ(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
         }
+
+        public static bool ValidarCPF(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+
+    }
 }
 
 
